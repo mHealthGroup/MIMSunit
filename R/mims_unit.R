37c0f9c68d4268963e3d5f4d4877d8c025d2d0dc
qutang <tqshelly@gmail.com>
2018-10-24 13:33:07 -0400

    Refactor mims_unit.R

--- R/mims_unit.R
+++ R/mims_unit.R
@@ -13,158 +13,217 @@
 #' @param integration the integration method to be used: 'trapz', 'absoluteMeanByPoints'.
 #' @param allow_truncation use zero truncation or not, default is TRUE
 #' @export
-mims_unit <- function(df, breaks = "5 sec", range, noise_level = 0.03, k = 0.05, spar = 0.6, filter_type = "butter", cutoffs = c(0.2, 5), integration = "trapz",
-    axes = c(2, 3, 4), use_extrapolation = TRUE, use_interpolation = TRUE, use_filtering = TRUE, combination = "sum", vm_after_extrapolation = FALSE,
-    allow_truncation = TRUE, output_per_axis = FALSE, output_orientation = FALSE, breaks_for_orientation = NULL, before_df = NULL, after_df = NULL)
-    {
-
+mims_unit <-
+  function(df,
+           breaks = "5 sec",
+           range,
+           noise_level = 0.03,
+           k = 0.05,
+           spar = 0.6,
+           filter_type = "butter",
+           cutoffs = c(0.2, 5),
+           integration = "trapz",
+           axes = c(2, 3, 4),
+           use_extrapolation = TRUE,
+           use_interpolation = TRUE,
+           use_filtering = TRUE,
+           combination = "sum",
+           vm_after_extrapolation = FALSE,
+           allow_truncation = TRUE,
+           output_per_axis = FALSE,
+           output_orientation = FALSE,
+           breaks_for_orientation = NULL,
+           before_df = NULL,
+           after_df = NULL)
+  {
     # save the start and stop time of original df
     start_time <- lubridate::floor_date(df[1, 1], unit = "seconds")
-    stop_time <- lubridate::floor_date(df[nrow(df), 1], unit = "seconds")
+    stop_time <-
+      lubridate::floor_date(df[nrow(df), 1], unit = "seconds")
 
     # concatenate with before and after df
     if (is.data.frame(before_df))
     {
-        df <- rbind(before_df, df)
+      df <- rbind(before_df, df)
     }
     if (is.data.frame(after_df))
     {
-        df <- rbind(df, after_df)
+      df <- rbind(df, after_df)
     }
 
     # apply extrapolation algorithm
     if (use_extrapolation)
     {
-        extrapolatedData <- extrapolate.data.frame(df, range, noise_level, k, spar)
+      extrapolated_data <-
+        extrapolate.data.frame(df, range, noise_level, k, spar)
     } else if (use_interpolation)
     {
-        extrapolatedData <- interpolate_signal(df, sr = 100, method = "spline_natural")
+      extrapolated_data <-
+        interpolate_signal(df, sr = 100, method = "spline_natural")
     } else
     {
-        extrapolatedData <- df
+      extrapolated_data <- df
     }
 
     if (vm_after_extrapolation)
     {
-        extrapolatedData <- vector_magnitude(extrapolatedData, axes = axes)
+      extrapolated_data <-
+        vector_magnitude(extrapolated_data, axes = axes)
     }
 
-    sr <- sampling_rate(extrapolatedData)
+    sr <- sampling_rate(extrapolated_data)
     # Resample to a consistent sampling rate
     if (FALSE)
     {
-        resampledData <- change_sampling_rate(extrapolatedData, origSr = sr, newSr = resample)
-        # update to the new sampling rate
-        sr <- resample
+      resampled_data <-
+        change_sampling_rate(extrapolated_data, origSr = sr, newSr = resample)
+      # update to the new sampling rate
+      sr <- resample
     } else
     {
-        resampledData <- extrapolatedData
+      resampled_data <- extrapolated_data
     }
 
     # store -1 values separately
-    row_abnormal <- rep(FALSE, nrow(resampledData))
-    for (i in 2:ncol(resampledData))
+    row_abnormal <- rep(FALSE, nrow(resampled_data))
+    for (i in 2:ncol(resampled_data))
     {
-        row_abnormal <- row_abnormal | resampledData[[i]] < -150
+      row_abnormal <- row_abnormal | resampled_data[[i]] < -150
     }
 
-    abnormalData <- resampledData[row_abnormal, ]
-    normalData <- resampledData[!row_abnormal, ]
+    abnormal_data <- resampled_data[row_abnormal,]
+    normal_data <- resampled_data[!row_abnormal,]
 
     # Apply filter cascade
     if (use_filtering)
     {
-        if (filter_type == "butter")
-        {
-            filteredData <- iir(normalData, Fs = sr, Fc = cutoffs, order = 4, type = "pass", filter_type = "butter")
-        } else if (filter_type == "bessel")
-        {
-            filteredData <- average_removal(normalData, Fs = sr, order = 0.5)
-            filteredData <- filteredData[[1]]
-            filteredData <- bessel(filteredData, Fs = sr, Fc = cutoffs[2] * 2, order = 8)
-        } else if (filter_type == "ellip")
-        {
-            filteredData <- iir(normalData, Fs = sr, Fc = cutoffs, order = 4, type = "pass", filter_type = "ellip")
-        }
+      if (filter_type == "butter")
+      {
+        filtered_data <-
+          iir(
+            normal_data,
+            Fs = sr,
+            Fc = cutoffs,
+            order = 4,
+            type = "pass",
+            filter_type = "butter"
+          )
+      } else if (filter_type == "bessel")
+      {
+        filtered_data <- average_removal(normal_data, sr = sr, order = 0.5)
+        filtered_data <- filtered_data[[1]]
+        filtered_data <-
+          bessel(
+            filtered_data,
+            sr = sr,
+            cutoff_freq = cutoffs[2] * 2,
+            order = 8
+          )
+      } else if (filter_type == "ellip")
+      {
+        filtered_data <-
+          iir(
+            normal_data,
+            Fs = sr,
+            Fc = cutoffs,
+            order = 4,
+            type = "pass",
+            filter_type = "ellip"
+          )
+      }
     } else
     {
-        filteredData <- normalData
+      filtered_data <- normal_data
     }
 
     # sort by timestamp
-    colnames(abnormalData)[2:4] <- colnames(filteredData)[2:4]
-    filteredData <- rbind(filteredData, abnormalData)
-    filteredData <- filteredData[order(filteredData$HEADER_TIME_STAMP), ]
+    colnames(abnormal_data)[2:4] <- colnames(filtered_data)[2:4]
+    filtered_data <- rbind(filtered_data, abnormal_data)
+    filtered_data <-
+      filtered_data[order(filtered_data$HEADER_TIME_STAMP), ]
 
     # Compute orientations
     if (output_orientation)
     {
-        if (is.null(breaks_for_orientation))
-        {
-            breaks_for_orientation <- breaks
-        }
-        orientationData <- aggregate_for_orientation(filteredData, breaks = breaks_for_orientation)
+      if (is.null(breaks_for_orientation))
+      {
+        breaks_for_orientation <- breaks
+      }
+      orientation_data <-
+        aggregate_for_orientation(filtered_data,
+                                  breaks = breaks_for_orientation)
     } else
     {
-        orientationData <- NULL
+      orientation_data <- NULL
     }
 
     # Compute the AUC
-    integratedData <- aggregate_for_mims(filteredData, breaks = breaks, type = integration, rectify = TRUE)
+    integrated_data <-
+      aggregate_for_mims(filtered_data,
+                         breaks = breaks,
+                         type = integration,
+                         rectify = TRUE)
 
     if (vm_after_extrapolation)
     {
-        return(integratedData)
+      return(integrated_data)
     }
     # Compute vector magnitude
-    row_abnormal <- rep(FALSE, nrow(integratedData))
-    for (i in 2:ncol(integratedData))
+    row_abnormal <- rep(FALSE, nrow(integrated_data))
+    for (i in 2:ncol(integrated_data))
     {
-        row_abnormal <- row_abnormal | integratedData[[i]] < 0
+      row_abnormal <- row_abnormal | integrated_data[[i]] < 0
     }
     if (combination == "vm")
-        countsData <- vector_magnitude(integratedData, axes = axes) else if (combination == "sum")
-        {
-        countsData <- sum_up(integratedData, axes = axes)
+      mims_data <-
+      vector_magnitude(integrated_data, axes = axes)
+    else if (combination == "sum")
+    {
+      mims_data <- sum_up(integrated_data, axes = axes)
     } else
     {
-        countsData <- sum_up(integratedData, axes = axes)
+      mims_data <- sum_up(integrated_data, axes = axes)
     }
 
     if (output_per_axis)
     {
-        countsData <- cbind(countsData, integratedData[, 2:ncol(integratedData)])
-        colnames(countsData)[2:ncol(countsData)] <- c("MIMS_UNIT", "MIMS_UNIT_X", "MIMS_UNIT_Y", "MIMS_UNIT_Z")
-        countsData[row_abnormal, c(2, 3, 4, 5)] <- -0.01
+      mims_data <-
+        cbind(mims_data, integrated_data[, 2:ncol(integrated_data)])
+      colnames(mims_data)[2:ncol(mims_data)] <-
+        c("MIMS_UNIT", "MIMS_UNIT_X", "MIMS_UNIT_Y", "MIMS_UNIT_Z")
+      mims_data[row_abnormal, c(2, 3, 4, 5)] <- -0.01
     } else
     {
-        colnames(countsData)[2] <- "MIMS_UNIT"
-        countsData[row_abnormal, 2] <- -0.01
+      colnames(mims_data)[2] <- "MIMS_UNIT"
+      mims_data[row_abnormal, 2] <- -0.01
     }
 
 
     if (allow_truncation)
     {
-        truncate_indices <- countsData[, 2:ncol(countsData)] > 0 & (countsData[, 2:ncol(countsData)] <= (1e-04 * break_str_to_sample_size(NULL, breaks,
-            sr)/sr))
-        truncate_indices <- data.frame(truncate_indices)
-        countsData[, 2:ncol(countsData)] <- sapply(1:(ncol(countsData) - 1), function(n)
+      truncate_indices <-
+        mims_data[, 2:ncol(mims_data)] > 0 &
+        (mims_data[, 2:ncol(mims_data)] <=
+           (1e-04 * break_str_to_sample_size(NULL, breaks, sr) / sr))
+      truncate_indices <- data.frame(truncate_indices)
+      mims_data[, 2:ncol(mims_data)] <-
+        sapply(1:(ncol(mims_data) - 1), function(n)
         {
-            countsData[truncate_indices[, n], n + 1] <- 0
-            return(countsData[, n + 1])
+          mims_data[truncate_indices[, n], n + 1] <- 0
+          return(mims_data[, n + 1])
         })
     }
 
     # only keep data between start and end time
-    keep_mask <- countsData[[1]] >= start_time & countsData[[1]] < stop_time
-    countsData <- countsData[keep_mask, ]
+    keep_mask <-
+      mims_data[[1]] >= start_time & mims_data[[1]] < stop_time
+    mims_data <- mims_data[keep_mask, ]
 
-    # countsData = countsData[!is.na(countsData[,2]),]
     if (output_orientation)
     {
-        return(list(mims = countsData, orientation = orientationData))
+      return(list(mims = mims_data, orientation = orientation_data))
     } else
     {
-        return(countsData)
+      return(mims_data)
     }
-}
+  }
