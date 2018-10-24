2db68ab040940e1164de87be1d689821053384ae
qutang <tqshelly@gmail.com>
2018-10-24 12:10:18 -0400

    Refactor make_data.R

--- R/make_data.R
+++ R/make_data.R
@@ -3,16 +3,23 @@
 #' @export
 #' @importFrom plyr colwise
 #' @importFrom stats rnorm
-crop_grange = function(df, range = NULL, noise_std = 0.03){
-  if(!is.null(range)){
-    upper = range[2]
-    lower = range[1]
-    cropFun = plyr::colwise(function(colData){
-      colData[colData > upper] = upper + stats::rnorm(sum(colData > upper), 0, noise_std)
-      colData[colData < lower] = lower + stats::rnorm(sum(colData < lower), 0, noise_std)
-      return(colData)
+crop_grange <- function(df,
+                        range = NULL,
+                        noise_std = 0.03)
+{
+  if (!is.null(range))
+  {
+    upper <- range[2]
+    lower <- range[1]
+    crop_fun <- plyr::colwise(function(col_data)
+    {
+      col_data[col_data > upper] <-
+        upper + stats::rnorm(sum(col_data > upper), 0, noise_std)
+      col_data[col_data < lower] <-
+        lower + stats::rnorm(sum(col_data < lower), 0, noise_std)
+      return(col_data)
     })
-    df[,2:ncol(df)] = cropFun(df[2:ncol(df)])
+    df[, 2:ncol(df)] <- crop_fun(df[2:ncol(df)])
   }
   return(df)
 }
@@ -20,12 +27,16 @@ crop_grange = function(df, range = NULL, noise_std = 0.03){
 #' @name make_sensor_data
 #' @title make sensor data given range and sampling rate and old sensor data
 #' @export
-make_sensor_data = function(oldData, new_range, new_sr){
-  if(sampling_rate(oldData) != new_sr){
-    newData = interpolate(oldData, method = "spline_natural", sr = new_sr)
-  }else{
-    newData = oldData
+make_sensor_data <- function(old_data, new_range, new_sr)
+{
+  if (sampling_rate(old_data) != new_sr)
+  {
+    new_data <-
+      interpolate(old_data, method = "spline_natural", sr = new_sr)
+  } else
+  {
+    new_data <- old_data
   }
-  newData = crop_grange(newData, range = new_range)
-  return(newData)
+  new_data <- crop_grange(new_data, range = new_range)
+  return(new_data)
 }
