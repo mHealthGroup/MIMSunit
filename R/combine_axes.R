5fe062068bb007dd2857d600a55a3bc78993373d
qutang <tqshelly@gmail.com>
2018-10-25 12:10:06 -0400

    Add documentation for combination functions and update website.

--- R/combine_axes.R
+++ R/combine_axes.R
@@ -1,7 +1,31 @@
-#' @name vector_magnitude
-#' @title Compute the vector magnitude value of sensor data.
+#' Vector magnitude of multi-channel signal.
+#'
+#' \code{vector_magnitude} computes the vector magnitude value for each sample
+#' (row) of a multi-channel signal.
+#'
+#' This function takes a dataframe of a multi-channel signal as input, and then
+#' computes the 2-norm (vector magnitude) for each row and returns a transformed
+#' dataframe with two columns.
+#'
+#' @section How is it used in MIMS-unit algorithm?: This function is not used in
+#'   the released version of MIMS-unit algorithm, but was used to compare the
+#'   alternative \code{\link{sum_up}} method when combining MIMS-unit values on
+#'   each axis into a single value.
+#'
+#' @param df dataframe. multi-channel signal, with the first column being the
+#'   timestamp in POSXct format.
+#' @param axes numerical vector. Specify the column indices for each axis. When
+#'   this value is NULL, the function assumes the axes are starting from column
+#'   2 to the end. Default is \code{NULL}.
+#'
+#' @return dataframe. The transformed dataframe will have the same number of
+#'   rows as input dataframe but only two columns, with the first being
+#'   timestamps and second being the vector magnitude values.
+#'
+#' @seealso \code{\link{sum_up}}
+#' @family combination functions
 #' @export
-#' @param df input dataframe that matches specification.
+#'
 vector_magnitude <- function(df, axes = NULL)
 {
   n_cols <- ncol(df)
@@ -25,10 +49,33 @@ vector_magnitude <- function(df, axes = NULL)
   return(vm_data)
 }
 
-#' @name sum_up
-#' @title Compute the sum up value of sensor data.
+#' Sum of multi-channel signal.
+#'
+#' \code{sum_up} computes the sum up value for each sample (row) of a
+#' multi-channel signal.
+#'
+#' This function takes a dataframe of a multi-channel signal as input, and then
+#' computes the sum of each row and returns a transformed dataframe with two
+#' columns.
+#'
+#' @section How is it used in MIMS-unit algorithm?: This function is used to
+#'   combine MIMS-unit values on each axis into a single value after aggregating
+#'   over each epoch using \code{\link{aggregate_for_mims}}.
+#'
+#' @param df dataframe. multi-channel signal, with the first column being the
+#'   timestamp in POSXct format.
+#' @param axes numerical vector. Specify the column indices for each axis. When
+#'   this value is NULL, the function assumes the axes are starting from column
+#'   2 to the end. Default is \code{NULL}.
+#'
+#' @return dataframe. The transformed dataframe will have the same number of
+#'   rows as input dataframe but only two columns, with the first being
+#'   timestamps and second being the sum up values.
+#'
+#' @seealso \code{\link{vector_magnitude}}
+#' @family combination functions
 #' @export
-#' @param df input dataframe that matches specification.
+#'
 sum_up <- function(df, axes = NULL)
 {
   n_cols <- ncol(df)
