654b2114b1a88f05fb58c8dc8426659564bdaa93
qutang <tqshelly@gmail.com>
2020-02-13 17:30:27 -0500

    build: Remove unused developer scripts. Now every deployment, building and checking will be handled by github actions.

--- build_package.R
+++
@@ -1,5 +0,0 @@
-# build source and binary packages
-renv::deactivate()
-devtools::build(path = 'dists/', binary = TRUE, vignettes = TRUE, manual = TRUE)
-devtools::build(path = 'dists/', vignettes = TRUE, manual = TRUE, binary = FALSE)
-renv::activate()
--- build_website.R
+++
@@ -1,7 +0,0 @@
-
-# renv does not work well with callr which was used by devtools and pkgdown, so use this workaround
-# https://github.com/r-lib/callr/issues/139
-system("Rcmd.exe INSTALL --no-multiarch --with-keep.source .")
-roxygen2::roxygenize('.', roclets = c('rd', 'collate', 'namespace', 'vignette'))
-pkgdown::build_site(examples = TRUE, lazy = FALSE, new_process = FALSE, devel = TRUE, preview = TRUE, )
-remove.packages('MIMSunit', lib = .libPaths()[1])
