99054765145a9e3e40765561418b11deaeca30be
qutang <tqshelly@gmail.com>
2020-05-14 11:19:04 -0400

    fix: Add options to shiny_app to support passing environment options before starting the shiny app

--- R/app.R
+++ R/app.R
@@ -7,12 +7,14 @@
 #' graphical interface to load local files, call \code{\link{mims_unit_from_files}}
 #' and display the results as an interactive graph.
 #'
+#' @param options The options passed to \code{\link[shiny]{shinyApp}}.
+#'
 #' @family Top level API functions
 #' @export
 #' @examples
 #'   \dontrun{
 #'   shiny_app()
 #'   }
-shiny_app <- function() {
-  shiny::runApp(appDir = system.file("app", package='MIMSunit'))
+shiny_app <- function(options = list()) {
+  shiny::shinyAppDir(appDir = system.file("app", package='MIMSunit'), options = options)
 }
