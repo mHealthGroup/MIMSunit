9be1217891cee4f730a7c26b4b0ddf1937394b62
qutang <tqshelly@gmail.com>
2020-05-15 11:42:10 -0400

    build: Add script to pack the shiny app as an electron app

---
+++ build_electron.R
@@ -0,0 +1,11 @@
+
+electricShine::electrify(app_name = "MIMSunitApp",
+                         short_description = "MIMS unit app",
+                         semantic_version = "0.9.0",
+                         build_path = "./dist",
+                         cran_like_url = "https://cran.r-project.org",
+                         function_name = "shiny_app",
+                         git_host = 'github',
+                         git_repo = 'mHealthGroup/MIMSunit',
+                         package_install_opts = list(type = "binary"),
+                         permission = TRUE)
